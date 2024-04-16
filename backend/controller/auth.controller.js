b54af3c84a0225dbcd6694da5f514881ca4caca1
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2024-04-16 18:07:15 +0545

    ready for testing 'postman'

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (req,res) =>{
+    res.send("signup user");
+}
+export const login = (req,res) =>{
+    res.send("login user");
+}
+export const logout = (req,res) =>{
+    res.send("logout user");
+}
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
-const express = require('express');
+import express from 'express';
 const app = express();
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import authRoutes from './routes/auth.routes.js'
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(8000, () => 
-console.log('server running on port 8000'));
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => 
+console.log(`server running on port ${PORT}`));
 
 app.get("/",(req, res) => {
     //root route localhost:8000/
-    res.send("Hello world!!!!");
+    res.send("Hello world!!");
 });
