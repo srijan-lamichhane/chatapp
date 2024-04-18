9a8acfe4f8fe25667477cefe6a64a0adc5041b60
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2024-04-19 00:07:04 +0545

    feat(db): connected to mongodb

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,6 +1,11 @@
-export const signup = (req,res) =>{
-    res.send("signup user");
-}
+export const signup = async(req,res) =>{
+    try{
+        const {fullName, username, password, confirmPassword, gender} = req.body;
+    }
+    catch(error){
+        
+    }
+};
 export const login = (req,res) =>{
     res.send("login user");
 }
--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import authRoutes from './routes/auth.routes.js'
+import { connect } from 'mongoose';
+import connectToMongoDB from './db/connectToMongoDB.js';
 
 const PORT = process.env.PORT || 8000;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => 
-console.log(`server running on port ${PORT}`));
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`server running on port ${PORT}`)
+});
 
 app.get("/",(req, res) => {
     //root route localhost:8000/
