0bb77abd07d266889b476a2ea347dd1102beac5f
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2024-04-20 17:33:57 +0545

    feat: signupEndpoint

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,9 +1,41 @@
 export const signup = async(req,res) =>{
     try{
         const {fullName, username, password, confirmPassword, gender} = req.body;
+        // to check if the password match
+        if(password!==confirmPassword){
+            return res.status(400).json({message: "passwords do not match"});
+        }
+        // to check if user already exists
+        const user = await User.findOne({username})
+        if(user){
+            return res.status(400).json({message: "user already exists"});
+        }
+        // to hash the password
+
+        //avatar: avatar-placeholder.iran.liara.run/
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        // to create a new user
+        const newUser = new User({
+            fullName, 
+            username,
+            password,
+            gender,
+            profilePic: gender === "male"? boyProfilePic : girlProfilePic
+        });
+        await newUser.save(); // to save the user to the database
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            profilePic: newUser.profilePic,
+        });                 
+
     }
     catch(error){
-        
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({message: "Internal server error"})
     }
 };
 export const login = (req,res) =>{
--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,14 @@ const PORT = process.env.PORT || 8000;
 
 app.use("/api/auth", authRoutes);
 
+app.use(express.json()); // to parse the incoming request with JSON payloads.
+
 app.listen(PORT, () => {
     connectToMongoDB();
     console.log(`server running on port ${PORT}`)
 });
 
-app.get("/",(req, res) => {
-    //root route localhost:8000/
-    res.send("Hello world!!");
-});
+// app.get("/",(req, res) => {
+//     res.send("Hello world!!");
+// });
+
