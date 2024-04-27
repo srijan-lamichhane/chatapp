3c61c0e3c942fe2687afc9377fc4cff206586510
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2024-04-27 10:12:01 +0545

    feat: get messages

--- backend/controller/message.controller.js
+++ backend/controller/message.controller.js
@@ -2,16 +2,16 @@ import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
 
 export const sendMessage = async (req, res) => {
-    try{
-        const {message} = req.body;
-        const {id: receiverId} = req.params;
+    try {
+        const { message } = req.body;
+        const { id: receiverId } = req.params;
         const senderId = req.user._id;
         //1. find the conversation between the sender and receiver
-        let conversation = await  Conversation.findOne({
-            participants: { $all: [senderId, receiverId]},
+        let conversation = await Conversation.findOne({
+            participants: { $all: [senderId, receiverId] },
         });
         //2. if conversation doesn't exist, create a new conversation
-        if(!conversation){
+        if (!conversation) {
             conversation = await Conversation.create({
                 participants: [senderId, receiverId],
             });
@@ -23,11 +23,13 @@ export const sendMessage = async (req, res) => {
             message,
         });
         //4. push the new message to the conversation
-        if(newMessage){
+        if (newMessage) {
             conversation.messages.push(newMessage);
         }
 
-        res.status(201).json({newMessage});
+        res.status(201).json({ newMessage });
+
+        //socket I/O function will go here
 
         //5. save the conversation to db
         // await conversation.save();
@@ -35,8 +37,32 @@ export const sendMessage = async (req, res) => {
 
         await Promise.all([conversation.save(), newMessage.save()]);
     }
-    catch(error){
+    catch (error) {
         console.log("Error in sendMessage controller: ", error.message);
-        res.status(500).json({error: "Internal Server Error"});
+        res.status(500).json({ error: "Internal Server Error" });
     }
 };
+
+export const getMessage = async (req, res) => {
+    try {
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, receiverId] },
+        }).populate("messages");  // Not Reference but acutal messages
+
+        // if conversation doesn't exist, return empty array
+        if(!conversation){
+            return res.status(200).json([]);
+        }
+        // if conversation exists, return the messages
+        const messages = conversation.messages;
+        res.status(200).json(messages);
+
+    }
+    catch (error) {
+        console.log("Error in getMessage controller: ", error.message);
+        res.status(500).json({error: "Internal Server Error"})
+    }
+}
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { sendMessage } from '../controller/message.controller.js';
+import { getMessage } from '../controller/message.controller.js';
 import protectRoute from '../middleware/protectRoute.js';
 //If we're exporting protectRoute as the default export from protectRoute.js, you should import it without curly braces {}
 
 const router = express.Router();
 router.post("/send/:id", protectRoute, sendMessage);
+router.get("/:id", protectRoute, getMessage);
 
 export default router;
