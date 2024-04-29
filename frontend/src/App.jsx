bfaf428faeaf79190b9a56968e59da74f789f107
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2024-04-29 16:23:50 +0545

    feat: login signup pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-
 import './App.css'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
 
 function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Login />
+      <Signup />
     </div>
 
   )
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -2,10 +2,79 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+      <div className='w-full p-6 rounded-lg shadow-md bg-slate-800 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-1'>
+        <h1 className='text-3xl font-semibold text-center text-gray-300'>
+          Login
+          <span className='text-blue-500'> ChatApp</span>
+        </h1>
+
+        <form>
+          <div>
+            <label className='label p-2'>
+              <span className='text-base label-text'>Username</span>
+            </label>
+            <input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' />
+          </div>
+
+          <div>
+            <label className='label'>
+              <span className='text-base label-text'>Password</span>
+            </label>
+            <input
+              type='password'
+              placeholder='Enter Password'
+              className='w-full input input-bordered h-10'
+            />
+          </div>
+          <a href='#' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+            {"Don't"} have an account?
+          </a>
+
+          <div>
+            <button className='btn btn-block btn-sm mt-2'>Login</button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login
+
+//only frontend
+{/* <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+<div className='w-full p-6 rounded-lg shadow-md bg-slate-800 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-1'>
+  <h1 className='text-3xl font-semibold text-center text-gray-300'>
+    Login
+    <span className='text-blue-500'> ChatApp</span>
+  </h1>
+
+  <form>
+    <div>
+      <label className='label p-2'>
+        <span className='text-base label-text'>Username</span>
+      </label>
+      <input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' />
+    </div>
+
+    <div>
+      <label className='label'>
+        <span className='text-base label-text'>Password</span>
+      </label>
+      <input
+        type='password'
+        placeholder='Enter Password'
+        className='w-full input input-bordered h-10'
+      />
+    </div>
+    <a href='#' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+      {"Don't"} have an account?
+    </a>
+
+    <div>
+      <button className='btn btn-block btn-sm mt-2'>Login</button>
+    </div>
+  </form>
+</div>
+</div> */}
