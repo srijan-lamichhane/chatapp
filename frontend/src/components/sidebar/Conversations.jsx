935d247cc9adf48649f981be56926e411c00f14b
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2024-08-30 20:36:53 +0545

    feat: getConversation II

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,18 +1,31 @@
 import useGetConversations from "../../hooks/useGetConversations";
 import Conversation from "./Conversation";
+import { getRandomEmoji } from "../../utils/emojis";
 
 const Conversations = () => {
-	const {loading, conversations} = useGetConversations();
-	console.log("CONVERSATIONS:", conversations);
-	
+	const { loading, conversations } = useGetConversations();
+
+	// Check if conversations has a users property
+	const users = conversations.users || [];
+
+	// console.log("USERS:", users);
+
 	return (
 		<div className='py-2 flex flex-col overflow-auto'>
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
+			{users.length === 0 ? (
+				<p>No conversations found.</p>
+			) : (
+				users.map((conversation, idx) => (
+					<Conversation
+						key={conversation._id}
+						conversation={conversation}
+						emoji={getRandomEmoji()}
+						lastIdx={idx === users.length - 1}
+					/>
+				))
+			)}
+
+			{loading ? <span className='loading loading-spinner'></span> : null}
 		</div>
 	);
 };
--- frontend/src/zustand/useConversation.js
+++ frontend/src/zustand/useConversation.js
@@ -1,4 +1,4 @@
-import {create} from zustand;
+import {create} from 'zustand';
 
 const useConversation = create ((set) => ({
     selectedConversation : null,
