fc5a8d94131dc4ff73e06739000d6a2b679d52a5
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2024-09-07 23:31:44 +0545

    feat: socket.io (online user status)

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -11,17 +11,27 @@ const io = new Server(server, {
         origin: ["http://localhost:3000"],
         methods: ["GET", "POST"],
     }
-
-})
+});
+// adding the object
+const userSocketMap = {}; // {userId(key): socketId(value)}
 
 // Listen for connection
 io.on('connection', (socket) => {
     console.log('a user connected', socket.id);
 
+    const userId = socket.handshake.query.userId;
+    if(userId != 'undefined') userSocketMap[userId] = socket.id;
+
+    //io.emit() is used to send the event to all the clients connected to the server.
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+        
+
     // socket.on() is used to listen for the event from the client side.
     socket.on('disconnect', () => {
         console.log('user disconnected', socket.id);
-    })
-})
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    });
+});
 
 export {app, io, server};
